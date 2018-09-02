59bba91e48c7b45e4609580039d3063c0e25653c
iceinto <iceinto@gmail.com>
2018-09-02 17:01:02 +0800

    <优化><是否为空判断><参考：github.com/stretchr/testify/assert>

--- empty.go
+++ empty.go
@@ -1,27 +1,46 @@
 package underscorego
 
-import "fmt"
+import "reflect"
 
 // 判断是否为空
-func (u *U) IsEmpty(i interface{}) bool{
-	var isEmpty bool
-	isEmpty = false
-	switch i.(type) {
-	case string:
-		if i == "" {
-			isEmpty = true
-		}
-		break
-	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
-		str := fmt.Sprintf("%d", i)
-		if str == "0" || str == "-0" {
-			isEmpty = true
-		}
-		break
-	case []string:
-		if len(i.([]string)) == 0 {
-			isEmpty = true
-		}
+func (u *U) IsEmpty(i interface{}) bool {
+
+	// get nil case out of the way
+	if i == nil {
+		return true
 	}
-	return isEmpty
+
+	objValue := reflect.ValueOf(i)
+
+	switch objValue.Kind() {
+	// collection types are empty when they have no element
+	case reflect.Array, reflect.Chan, reflect.Map, reflect.Slice:
+		return objValue.Len() == 0
+		// pointers are empty if nil or if the value they point to is empty
+	case reflect.Ptr:
+		if objValue.IsNil() {
+			return true
+		}
+		deref := objValue.Elem().Interface()
+		return u.IsEmpty(deref)
+		// for all other types, compare against the zero value
+	default:
+		zero := reflect.Zero(objValue.Type())
+		return reflect.DeepEqual(i, zero.Interface())
+	}
+}
+
+// 判断是否是 Nil
+func (u *U) IsNil(object interface{}) bool {
+	if object == nil {
+		return true
+	}
+
+	value := reflect.ValueOf(object)
+	kind := value.Kind()
+	if kind >= reflect.Chan && kind <= reflect.Slice && value.IsNil() {
+		return true
+	}
+
+	return false
 }
--- empty_test.go
+++ empty_test.go
@@ -7,18 +7,18 @@ import (
 )
 
 // 判断是否为空
-func TestIsEmpty(t *testing.T) {
+func TestU_IsEmpty(t *testing.T) {
 	var u U
 	// 测试字符串为空
 	is := u.IsEmpty("")
-	assert.Equal(t, is, true)
+	assert.Equal(t, is, true, "空字符串判断")
 	// 测试字符串不为空
 	is = u.IsEmpty("test")
-	assert.Equal(t, is, false)
+	assert.Equal(t, is, false, "非空字符串判断")
 	is = u.IsEmpty(0)
-	assert.Equal(t, is, true)
+	assert.Equal(t, is, true, "0判断")
 	is = u.IsEmpty(1)
-	assert.Equal(t, is, false)
+	assert.Equal(t, is, false, "1判断")
 	var ii int32
 	ii = 0
 	is = u.IsEmpty(ii)
@@ -30,4 +30,15 @@ func TestIsEmpty(t *testing.T) {
 	strArr2 := []string{""}
 	is = u.IsEmpty(strArr2)
 	assert.Equal(t, is, false)
+	is = u.IsEmpty(make(chan struct{}))
+	assert.Equal(t, is, true)
+}
+
+func TestU_IsNil(t *testing.T) {
+	var u U
+	is := u.IsNil(nil)
+	assert.Equal(t, is, true, "nil 是否是nil判断")
+	// 字符串判断
+	is = u.IsNil("")
+	assert.Equal(t, is, false, "空字符串 是否是nil判断")
 }
